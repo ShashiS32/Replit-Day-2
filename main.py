49a6ffe0076fcc8a57511d1d239df30297c5cb93
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-03-27 00:31:27 +0000

    f string

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-x = "Hello Wolrd"
-print(len(x))
+numclasses = input("How many classes do you have? ") 
+averageclassestime = input("How many minutes do you spend on your classes for homework? ")
+
+numclasses = int(numclasses)
+averageclassestime = int(averageclassestime)
+
+print(f"Your average time spent on homework is {round(averageclassestime /numclasses , 1)} minutes per class.")

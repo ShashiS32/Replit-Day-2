4f86d78d7f4fda1fe85afe865744642a4f6fddb2
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-03-27 00:31:53 +0000

    challenge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numclasses = input("How many classes do you have? ") 
-averageclassestime = input("How many minutes do you spend on your classes for homework? ")
+age = 15
 
-numclasses = int(numclasses)
-averageclassestime = int(averageclassestime)
+weeks = age * 52
 
-print(f"Your average time spent on homework is {round(averageclassestime /numclasses , 1)} minutes per class.")
+average_weeks = 4056
+
+print (f"You have apprixmatley {average_weeks - weeks} weeks left to live")
